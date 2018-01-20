7e2879a14de4e82e671a572fd45df9ccf289f761
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-20 20:12:18 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,20 +8,18 @@ end
 
 def add_element_to_end_of_array(array, element)
   array << element
-  array
 end
 
 def add_element_to_start_of_array(array, element)
   array.unshift(element)
-  array
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop()
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
